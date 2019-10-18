426d2e0274c911b3b48d8599685e81d390c92c0f
dhscheeshiyaoalicia <37992215+dhscheeshiyaoalicia@users.noreply.github.com>
2019-10-18 14:44:15 +0800

    Update t01primeshcflcm_factors2.py

--- t01primeshcflcm_factors2.py
+++ t01primeshcflcm_factors2.py
@@ -1,3 +1,22 @@
 # Generate a list containing factors of num.
 
-num = 
+num = 18
+actual_num = num
+start = 2
+prime_fact = []
+
+while start <= num:
+if num % start == 0:
+  prime_fact += [start]
+  num = num / start
+  else:
+    start += 1
+    
+print(prime_fact)
+   
+print(actual_num,end = ' ')
+print("=", end=' ')
+for index, i in enumerate(prime_factor):
+  print(i, end=' ')
+  if index, != len(prime_fact)-1:
+    print("x", end = ' ')
